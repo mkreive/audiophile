e9bd6d57f95e56d69101b7cb8a4fe0587047f6d1
mkreive <mkreive@gmail.com>
2023-02-22 11:02:00 +0200

    helmet and icon changed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet';
 import classNames from 'classnames/bind';
 import styles from './home-page.module.scss';
 import { Hero } from '../../components/Hero';
@@ -8,6 +9,9 @@ const cn = classNames.bind(styles);
 export default function HomePage() {
   return (
     <div className={cn('container')}>
+      <Helmet>
+        <title>Audiophile - Home</title>
+      </Helmet>
       <Hero />
       <MainLayout>
         {/* <ProductHighlightSection /> */}
