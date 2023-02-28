408c6b0c8d539ef33b89522f36085be315c62590
mkreive <mkreive@gmail.com>
2023-02-28 16:00:44 +0200

    card in progress

--- src/features/ProductHighlights/CustomCard.jsx
+++ src/features/ProductHighlights/CustomCard.jsx
@@ -8,16 +8,28 @@ const cn = classNames.bind(styles);
 export default function CustomCard({ product }) {
   return (
     <div className={cn('custom-card')}>
-      <div className={cn('custom-card--img')}>
+      <div className={cn('custom-card__img')}>
         <img src={product.image} alt={product.name} />
       </div>
-      <div className={cn('custom-card--text')}>
-        <Heading tag='h2' theme='white'>
+      <div className={cn('custom-card__text')}>
+        <Heading tag='h1' theme='white'>
           {product.name}
         </Heading>
         <Paragraph theme='gray'>{product.info}</Paragraph>
-        <Button theme='white'>See product</Button>
+        <Button theme='black'>See product</Button>
       </div>
+      <svg
+        width='944'
+        height='944'
+        xmlns='http://www.w3.org/2000/svg'
+        className={cn('custom-card__pattern')}
+      >
+        <g stroke='#FFF' fill='none' fillRule='evenodd' opacity='.202'>
+          <circle cx='472' cy='472' r='235.5' />
+          <circle cx='472' cy='472' r='270.5' />
+          <circle cx='472' cy='472' r='471.5' />
+        </g>
+      </svg>
     </div>
   );
 }
